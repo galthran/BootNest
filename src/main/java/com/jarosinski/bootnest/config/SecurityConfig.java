30d536710d3239c2aa91f96544162f4240eb2e1a
Galthran <rafal.jarosinski@gmail.com>
2024-12-28 15:53:46 +0100

    Implementing jdbc user authentication

--- src/main/java/com/jarosinski/bootnest/config/SecurityConfig.java
+++ src/main/java/com/jarosinski/bootnest/config/SecurityConfig.java
@@ -2,18 +2,23 @@ package com.jarosinski.bootnest.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
-    @Bean
+    /*@Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
 
          UserDetails john = User.builder().username("john").password("{noop}test123").roles("EMPLOYEE").build();
@@ -21,6 +26,11 @@ public class SecurityConfig {
          UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }*/
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
@@ -30,8 +40,10 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth ->
                 auth.requestMatchers("/daily-workout").permitAll()
                 .requestMatchers("/api/students/**").permitAll()
-                .requestMatchers("/api/employees/**").permitAll()
                 .requestMatchers("/api/student").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasAnyRole("EMPLOYEE", "MANAGER", "ADMIN")
+                .requestMatchers(HttpMethod.POST, "/api/employees/**").hasAnyRole( "MANAGER", "ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
             )
             .formLogin(AbstractAuthenticationFilterConfigurer::permitAll
--- src/main/resources/sql/spring-security.sql
+++ src/main/resources/sql/spring-security.sql
@@ -1,21 +1,19 @@
-CREATE TABLE "user" (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
+CREATE TABLE "users" (
+    username VARCHAR(50) NOT NULL PRIMARY KEY,
     password VARCHAR(250) NOT NULL,
-    enabled BOOLEAN NOT NULL  DEFAULT TRUE
+    enabled BOOLEAN NOT NULL DEFAULT TRUE
 );
 
-INSERT INTO "user" (username, password, enabled)
+INSERT INTO "users" (username, password, enabled)
 VALUES ('john', '{noop}test123', TRUE),
        ('mary', '{noop}test123', TRUE),
        ('susan', '{noop}test123', TRUE);
 
 CREATE TABLE "authorities"
 (
-    id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     authority VARCHAR(50) NOT NULL,
-    CONSTRAINT fk_authorities_users FOREIGN KEY (username) REFERENCES "user" (username)
+    CONSTRAINT fk_authorities_users FOREIGN KEY (username) REFERENCES "users" (username)
 );
 
 INSERT INTO "authorities" (username, authority)
